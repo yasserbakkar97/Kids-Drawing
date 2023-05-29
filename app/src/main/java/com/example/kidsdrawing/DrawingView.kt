7605d8d1ffc4dbf061a754d0ac0f8ad7e474af43
YasserBakkar <yasserbakkar97@gmail.com>
2023-05-29 18:55:43 +0300

    initial commit

---
+++ app/src/main/java/com/example/kidsdrawing/DrawingView.kt
@@ -0,0 +1,40 @@
+package com.example.kidsdrawing
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs){
+
+    private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap : Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0F
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20F
+
+
+    }
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
+
+}
