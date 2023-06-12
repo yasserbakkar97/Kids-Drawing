4c4127d10f59c598833147a1a307e45d88a9c965
YasserBakkar <yasserbakkar97@gmail.com>
2023-06-12 05:57:12 +0300

    Add requestPermission

--- app/src/main/java/com/example/kidsdrawing/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawing/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kidsdrawing
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,6 +23,37 @@ import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint : ImageButton? = null
+
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data!=null){
+                val imageBackground : ImageView = ivBackground
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+
+
+    private val requestPermission : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted){
+                    Toast.makeText(this, "Permission granted now you can read the storage files.", Toast.LENGTH_SHORT).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                }else{
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this, "Oops you just denied the permission.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +69,26 @@ class MainActivity : AppCompatActivity() {
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
     }
 
-
-
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        ) {
+            showRationaleDialog(
+                "Kids Drawing App",
+                "Kids Drawing App needs to Access your External Storage"
+            )
+        } else {
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            // TODO - Add writing external storage
+            ))
+        }
+    }
 
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
@@ -52,7 +109,6 @@ class MainActivity : AppCompatActivity() {
             drawing_view?.setSizeForBrush(30F)
             brushDialog.dismiss()
         }
-
         brushDialog.show()
     }
 
@@ -70,6 +126,22 @@ class MainActivity : AppCompatActivity() {
             )
             mImageButtonCurrentPaint = view
         }
-
     }
+    /**
+     * Shows rationale dialog for displaying why the app needs permission
+     * Only shown if the user has denied the permission request previously
+     */
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
 }
